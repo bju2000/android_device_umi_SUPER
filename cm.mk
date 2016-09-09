6008a3fbca88c3c2012d0c676ff49905e2822e43
Primo3 <ciao_notte@hotmail.com>
2016-09-09 19:11:05 +0200

    08-09-2016

--- cm.mk
+++ cm.mk
@@ -15,7 +15,7 @@
 #
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
