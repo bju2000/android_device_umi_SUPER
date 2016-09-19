1afd2e1df1c9b8adf7a02c5b27fa694f168b9ce3
Primo3 <ciao_notte@hotmail.com>
2016-09-19 22:25:12 +0200

    19-09-2016 #2

--- product/audio.mk
+++ product/audio.mk
@@ -1,10 +1,1 @@
 # Audio
-PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    audio.usb.default \
-    audio.r_submix.default \
-    audio_policy.default \
-    libaudio-resampler \
-    libtinycompress \
-    libtinyxml \
-    libtinyalsa
